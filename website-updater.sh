ec1d37d1085f7ab5e36161a43b4122dc0250cbfe
abitwitch <arrakis256@gmail.com>
2025-08-13 14:26:05 +0100

    working on updater

--- website-updater.sh
+++ website-updater.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")";
 curdir=$(pwd | xargs basename)
 gitreponame=$(basename `sudo -u witch git rev-parse --show-toplevel`)
 static_dir="/var/www/html/public_static"
-git_static_dir="$pwd/public_static"
+git_static_dir="$(pwd)/public_static"
 
 if [ "$curdir" = "$gitreponame" ]; then
   while :
@@ -15,7 +15,9 @@ if [ "$curdir" = "$gitreponame" ]; then
     git_static_dir_last_update=$(find $git_static_dir -type f -printf '%Ts\n' | sort -k1,1nr | head -1)
     if (( static_dir_last_update > git_static_dir_last_update)); then
       echo "Updateing git static folder"
-      rsync -avu --delete $static_dir $git_static_dir
+      rm -rf $git_static_dir/*
+      mv $static_dir/index.html $git_static_dir/index.html.gz
+      gzip -d $git_static_dir/index.html.gz
       sudo -u witch git pull
       gitdiff=$(sudo -u witch git diff --ignore-all-space -I"secret=.*\"")
       if [ "$gitdiff" != "" ]; then
@@ -29,6 +31,7 @@ if [ "$curdir" = "$gitreponame" ]; then
         echo "No changes since last run."
       fi
     fi
+    echo "sleeping for a minute..."
     sleep 60
   done
 else
